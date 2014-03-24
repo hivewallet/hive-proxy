6250fca8c56682d765f127c258d4e811af9cb330
Wei Lu <luwei.here@gmail.com>
2014-03-24 19:48:19 +0800

    allow cors for post

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require("express");
 var request = require('request')
 var app = express();
 
+function allowCrossDomain(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+  next()
+}
+
+app.use(allowCrossDomain)
 app.use(express.urlencoded())
 
 app.get('/*', function(req, res){
